0c334c1eb7b99601613919c8607e93bb9a5cb187
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-07-08 13:33:30 +0700

    Membuat Form Data Kontrak

---
+++ AlbumLagu/DataKontrak.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace AlbumLagu
+{
+    partial class DataKontrak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtidkontrak = new System.Windows.Forms.TextBox();
+            this.txtidartis = new System.Windows.Forms.TextBox();
+            this.txtidproduser = new System.Windows.Forms.TextBox();
+            this.txtnilaikontrak = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnopen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(217, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(404, 190);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID Kontrak";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID Artis";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ID Produser";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tanggal Lahir";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(213, 364);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Tanggal Berakhir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(213, 396);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Nilai Kontrak";
+            // 
+            // txtidkontrak
+            // 
+            this.txtidkontrak.Location = new System.Drawing.Point(348, 239);
+            this.txtidkontrak.Name = "txtidkontrak";
+            this.txtidkontrak.Size = new System.Drawing.Size(273, 26);
+            this.txtidkontrak.TabIndex = 7;
+            // 
+            // txtidartis
+            // 
+            this.txtidartis.Location = new System.Drawing.Point(348, 271);
+            this.txtidartis.Name = "txtidartis";
+            this.txtidartis.Size = new System.Drawing.Size(273, 26);
+            this.txtidartis.TabIndex = 8;
+            // 
+            // txtidproduser
+            // 
+            this.txtidproduser.Location = new System.Drawing.Point(348, 301);
+            this.txtidproduser.Name = "txtidproduser";
+            this.txtidproduser.Size = new System.Drawing.Size(273, 26);
+            this.txtidproduser.TabIndex = 9;
+            // 
+            // txtnilaikontrak
+            // 
+            this.txtnilaikontrak.Location = new System.Drawing.Point(348, 396);
+            this.txtnilaikontrak.Name = "txtnilaikontrak";
+            this.txtnilaikontrak.Size = new System.Drawing.Size(273, 26);
+            this.txtnilaikontrak.TabIndex = 12;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(217, 454);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 29);
+            this.btnadd.TabIndex = 13;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(379, 454);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 29);
+            this.btnsave.TabIndex = 14;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(546, 454);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 29);
+            this.btnclear.TabIndex = 15;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(348, 333);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(273, 26);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(348, 365);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(273, 26);
+            this.dateTimePicker2.TabIndex = 17;
+            // 
+            // btnopen
+            // 
+            this.btnopen.Location = new System.Drawing.Point(646, 24);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(75, 36);
+            this.btnopen.TabIndex = 18;
+            this.btnopen.Text = "Open";
+            this.btnopen.UseVisualStyleBackColor = true;
+            // 
+            // DataKontrak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 529);
+            this.Controls.Add(this.btnopen);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtnilaikontrak);
+            this.Controls.Add(this.txtidproduser);
+            this.Controls.Add(this.txtidartis);
+            this.Controls.Add(this.txtidkontrak);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "DataKontrak";
+            this.Text = "Data Kontrak";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtidkontrak;
+        private System.Windows.Forms.TextBox txtidartis;
+        private System.Windows.Forms.TextBox txtidproduser;
+        private System.Windows.Forms.TextBox txtnilaikontrak;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btnopen;
+    }
+}
