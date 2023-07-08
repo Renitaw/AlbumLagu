d16bb58ba44e7bb6500f77a0c3e14eca08fc6f1f
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-07-08 13:20:48 +0700

    Membuat Form Data Daftar Putar

---
+++ AlbumLagu/DataDaftarPutar.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace AlbumLagu
+{
+    partial class DataDaftarPutar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtiddaftarputar = new System.Windows.Forms.TextBox();
+            this.txtnamadaftar = new System.Windows.Forms.TextBox();
+            this.txtidpengguna = new System.Windows.Forms.TextBox();
+            this.txtidlagu = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnopen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(203, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(383, 195);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID Daftar putar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama Daftar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ID Pengguna";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 354);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ID Lagu";
+            // 
+            // txtiddaftarputar
+            // 
+            this.txtiddaftarputar.Location = new System.Drawing.Point(321, 249);
+            this.txtiddaftarputar.Name = "txtiddaftarputar";
+            this.txtiddaftarputar.Size = new System.Drawing.Size(265, 26);
+            this.txtiddaftarputar.TabIndex = 5;
+            // 
+            // txtnamadaftar
+            // 
+            this.txtnamadaftar.Location = new System.Drawing.Point(321, 282);
+            this.txtnamadaftar.Name = "txtnamadaftar";
+            this.txtnamadaftar.Size = new System.Drawing.Size(265, 26);
+            this.txtnamadaftar.TabIndex = 6;
+            // 
+            // txtidpengguna
+            // 
+            this.txtidpengguna.Location = new System.Drawing.Point(321, 315);
+            this.txtidpengguna.Name = "txtidpengguna";
+            this.txtidpengguna.Size = new System.Drawing.Size(265, 26);
+            this.txtidpengguna.TabIndex = 7;
+            // 
+            // txtidlagu
+            // 
+            this.txtidlagu.Location = new System.Drawing.Point(321, 348);
+            this.txtidlagu.Name = "txtidlagu";
+            this.txtidlagu.Size = new System.Drawing.Size(265, 26);
+            this.txtidlagu.TabIndex = 8;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(203, 406);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 32);
+            this.btnadd.TabIndex = 9;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(357, 406);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 32);
+            this.btnsave.TabIndex = 10;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(510, 406);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 32);
+            this.btnclear.TabIndex = 11;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // btnopen
+            // 
+            this.btnopen.Location = new System.Drawing.Point(604, 30);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(75, 27);
+            this.btnopen.TabIndex = 12;
+            this.btnopen.Text = "Open";
+            this.btnopen.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnopen);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtidlagu);
+            this.Controls.Add(this.txtidpengguna);
+            this.Controls.Add(this.txtnamadaftar);
+            this.Controls.Add(this.txtiddaftarputar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form3";
+            this.Text = "Data Daftar Putar";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtiddaftarputar;
+        private System.Windows.Forms.TextBox txtnamadaftar;
+        private System.Windows.Forms.TextBox txtidpengguna;
+        private System.Windows.Forms.TextBox txtidlagu;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnopen;
+    }
+}
