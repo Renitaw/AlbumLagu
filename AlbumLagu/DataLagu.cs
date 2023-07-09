9ac0dfce3e7e83321c9386b366674742dce422a1
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-07-09 10:05:19 +0700

    Membuat method untuk membuka dan menutup koneksi pada sql atau database

--- AlbumLagu/DataLagu.cs
+++ AlbumLagu/DataLagu.cs
@@ -25,6 +25,30 @@ namespace AlbumLagu
             refreshform();
         }
 
+        private void DataLagu_Load()
+        {
+            koneksi.Open();
+            SqlDataAdapter dataAdapter1 = new SqlDataAdapter(new SqlCommand("Select id_lagu, id_artis, id_produser, judul, durasi, genre from dbo.lagu", koneksi));
+            DataSet ds = new DataSet();
+            dataAdapter1.Fill(ds);
+
+            this.customerBindingSource.DataSource = ds.Tables[0];
+            this.txtidlagu.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "id_lagu", true));
+            this.txtidartis.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "id_artis", true));
+            this.txtidproduser.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "id_produser", true));
+            this.txtjudul.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "judul", true));
+            this.txtdurasi.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "durasi", true));
+            this.txtgenre.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "genre", true));
+            koneksi.Close();
+
+        }
+
         private void DataLagu_Load(object sender, EventArgs e)
         {
 
